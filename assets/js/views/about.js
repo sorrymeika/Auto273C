5288885c28fa90d0df3c5ec2460bca5a694df8e0
sun.lu <sun.lu@Ctrip.com>
2014-11-05 18:21:08 +0800

    add

---
+++ assets/js/views/about.js
@@ -0,0 +1,22 @@
+﻿define(['$','sl/sl','app'],function(require,exports,module) {
+    var $=require('$'),
+        sl=require('sl/sl'),
+        app=require('app'),
+        util=require('util');
+
+    module.exports=sl.Activity.extend({
+        template: 'views/about.html',
+        events: {},
+        onCreate: function() {
+            var that=this;
+
+            that.$('.js_type').html(_ACCOUNT_TYPE=="0"?"商务端 V 1.0版":"业务端 V 1.0版");
+        },
+        onStart: function() {
+        },
+        onResume: function() {
+        },
+        onDestory: function() {
+        }
+    });;
+});
---
+++ gen/com/sl/auto273c/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.sl.auto273c;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
